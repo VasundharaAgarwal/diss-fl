30345151015fa810f884297f718c673f2dd1ee6b
Vasundhara Agarwal <vasundhara6999@gmail.com>
2022-04-17 19:09:32 +0100

    run exps without slurm

---
+++ run_exp1.sh
@@ -0,0 +1,12 @@
+source /nfs-share/va308/diss-fl/env/bin/activate 
+
+zs=(0 0.5 0.75 1)
+
+for i in {0..4}
+    do
+    for z in "${zs[@]}"
+    do
+        command="python3 tfftutorialexp.py --noise_multiplier $z --run_num $i"
+        eval "$command"
+    done
+done
